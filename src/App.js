b8a73777a68e77208eca007a2fcdda7eeaa84c3d
CllaudiaB <buraanna27@gmail.com>
2025-05-20 10:43:52 +0200

    task3 - Initialize the main App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    axios({
+      method: "post",
+      url: "http://localhost:8000/api/auth/",
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .then(response => {
+        setIsLoggedIn(true);
+        setUserName(response.data.username);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="App">
+      {isLoggedIn ? <Dashboard /> : <Authentication />}
     </div>
   );
 }
