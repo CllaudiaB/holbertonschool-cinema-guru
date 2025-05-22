bbde1db738c6306ff56c59a4167dbf6820bce26f
CllaudiaB <buraanna27@gmail.com>
2025-05-22 18:14:50 +0200

    update task6 and add css file

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,21 +1,31 @@
-import "./navigation.css";
-import { faSignOut } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import './navigation.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 
 function Header({ userUsername, setIsLoggedIn }) {
+  function handleLogout() {
+    localStorage.removeItem('accessToken');
+    setIsLoggedIn(false);
+  }
 
-    function logout() {
-        localStorage.removeItem("accessToken");
-        setIsLoggedIn(false);
-    }
-
-    return (
-        <nav className="Header">
-            <img src="https://picsum.photos/100/100" alt="User picture" />
-            <p className="nav-text">Welcome, {userUsername}!</p>
-            <span onClick={logout}>{<FontAwesomeIcon icon={faSignOut}/>} Logout</span>
+  return (
+        <nav className="header">
+            <div className="header-left">
+                <h1 className="header-title">Cinema Guru</h1>
+            </div>
+            <div className="header-right">
+                <div className="user-info">
+                    <img src="https://picsum.photos/100/100" alt="User Avatar" />
+                    <p>Welcome, {userUsername}!</p>
+                </div>
+                <span className="logout-btn" onClick={handleLogout}>
+                    <FontAwesomeIcon icon={faRightFromBracket} /> Logout
+                </span>
+            </div>
         </nav>
     );
 }
+
 export default Header;
--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -1,16 +1,10 @@
 import "./dashboard.css";
 import Header from "../../components/navigation/Header";
-import SideBar from "../../components/navigation/SideBar";
 
 function Dashboard({ userUsername, setIsLoggedIn }) {
   return (
-    <div className="Dashboard">
-      <div className="Dashboard-header">
-        <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-      </div>
-      <div className="Dashboard-side-bar">
-        <SideBar  />
-      </div>
+    <div>
+      <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
     </div>
   );
 }
